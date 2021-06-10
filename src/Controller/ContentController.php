d9c744898e0d41097fed77358ec6c973348acd8a
Denis <ddg@abraham.tech>
2021-06-10 10:52:51 +0300

    start

---
+++ src/Controller/ContentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\ContentLoader;
+use stdClass;
+
+class ContentController extends AbstractController
+{
+    public function buildContent(Request $request, ContentLoader $contentLoader, $route)
+    {
+        /** @var stdClass $carouselData */
+        $carouselData = $contentLoader->getCarousel('/content/load/carousel');
+
+//        $carousel = new CarouselUnitList($carouselData->carouselUnits);
+
+        return $this->render('main.html.twig', [
+            'id' => 155,
+            'number' => $route,
+//            'carouselUnits' => $carousel->getCarouselUnits()
+        ]);
+    }
+}
---
+++ src/Service/ContentLoader.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ContentLoader
+{
+    /**
+     * @var HttpClientInterface
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $apiUrl;
+
+    /**
+     * ContentLoader constructor.
+     *
+     * @param HttpClientInterface $client
+     */
+    public function __construct(HttpClientInterface $client, string $apiUrl)
+    {
+        $this->client = $client;
+        $this->apiUrl = $apiUrl;
+    }
+
+    public function getCarousel(string $path)
+    {
+        $data = $this->client->request(
+            'GET',
+            $this->apiUrl . $path,
+        );
+
+        $content = $data->getContent();
+
+        return json_decode($content);
+    }
+}
