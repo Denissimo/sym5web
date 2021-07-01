d1923bb8420209d1d4749a3a0b961da16fa861f2
Denis <ddg@abraham.tech>
2021-07-01 16:27:09 +0300

    routes refactor

--- src/Controller/ArcgisController.php
+++ src/Controller/ArcgisController.php
@@ -11,4 +11,50 @@ class ArcgisController extends AbstractController
     {
             return $this->redirectToRoute('login');
     }
+
+
+    public function buildAirSituation(Request $request, string $tokenCookieName)
+    {
+        $cookieChecker = $request->cookies->get($tokenCookieName);
+        // var_dump ($cookieChecker);
+        // die;
+
+        if (!$cookieChecker) {
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('airsituation.html.twig', [
+            'id' => 155,
+            'route' => 'AirSituation',
+            'use_arcgis' => true
+        ]);
+    }
+
+    public function buildTracks(Request $request)
+    {
+        return $this->render('tracks.html.twig', [
+            'id' => 155,
+            'route' => 'Tracks',
+            'use_arcgis' => true
+        ]);
+    }
+
+    public function buildFlights(Request $request)
+    {
+        return $this->render('flights.html.twig', [
+            'id' => 155,
+            'route' => 'Flights',
+            'use_arcgis' => true
+        ]);
+    }
+
+
+    public function buildArchive(Request $request)
+    {
+
+        return $this->render('archive.html.twig', [
+            'id' => 155,
+            'route' => 'Archive'
+        ]);
+    }
 }
