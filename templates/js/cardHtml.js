131ba0430871e0eeab3c520f9457734c47e6f2a4
DVK <kdv@abraham.tech>
2021-08-25 19:08:12 +0300

    2508 cardHtml last

--- templates/js/cardHtml.js
+++ templates/js/cardHtml.js
@@ -1,106 +1,77 @@
- const flighthtml0 ='<li class="uav-list-item"><div class="uav-item-header">\
-  <span class="uav-item-status">';
-  const flighthtml1 ='</span>\
-  <button class="btn uav-btn-more" id="';
-  const flighthtml1_1='">Подробнее</button>\
-  </div>\
-  <div class="uav-item-body">';
- // <span class="uav-item-row uav-item-reg">123123123123ABBB</span>\
- const flighthtml2='<span class="uav-item-row uav-item-flight"><span class="uav-item-desc">Наименование</span>';
- const flighthtml3 ='</span>\
-  <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Старт</span>';
-  const flighthtml3_2 ='</span>\
-  <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Финиш</span>'; 
- const flighthtml4='</span>\
-                   </div>\
-                   <div class="uav-item-body" id="';
-  const flighthtml4_2='"></div>';
-  const flighthtml12_2='<input type="hidden" id="';
-  const flighthtml12_3='" value="';
-  const flighthtml12_4='"></input>';
-  const flighthtml13='</li>';
+ class cardHtml{
+  static    flighthtml = ['<li class="uav-list-item"><div class="uav-item-header"><span class="uav-item-status">',
+  '</span> <button class="btn uav-btn-more" id="',
+  '">Подробнее</button>  </div>  <div class="uav-item-body">',
+  '<span class="uav-item-row uav-item-flight"><span class="uav-item-desc">Наименование</span>',
+  '</span> <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Старт</span>',
+  '</span> <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Финиш</span>', 
+  '</span> </div> <div class="uav-item-body" id="',
+  '"></div>',
+  '<input type="hidden" id="',
+  '" value="',
+  '"></input>',
+  '</li>'];
 
 
 
 //detalowner
-  const flighthtml2 ='<span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Старт</span>';
-  const flighthtml2_2 ='</span>\
-   <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Финиш</span>';
-  const flighthtml2_3='</span>'; 
+  static flightdetal =['<span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Старт</span>',
+                       '</span><span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Финиш</span>',
+                       '</span>',
+                       '<span class="uav-item-row uav-item-flight" id="',
+                       '"><span class="uav-item-desc">Сер.номер БВС</span>',
+                       '<span class="uav-item-desc">Пользователь</span>',
+                       '</span>'];
 
-  const flighthtml3_1 ='<span class="uav-item-row uav-item-flight" id="'
-  
-  const flighthtml3_2='"><span class="uav-item-desc">Сер.номер БВС</span>';
-  
-  
-  const flighthtml3_3='</span>';
-
- //detal operator
-  const flighthtml2 ='<span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Старт</span>';
-  const flighthtml2_2 ='</span>\
-  <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Финиш</span>';
- const flighthtml2_3='</span>'; 
-
-
-  const flighthtml3_1 ='<span class="uav-item-row uav-item-flight" id="'
-  
-  const flighthtml3_2='"><span class="uav-item-desc">Рег.номер БВС</span>';
-  
-  const flighthtml3_4='<span class="uav-item-desc">Пользователь</span>';
-  const flighthtml3_3='</span>'; 
+ 
 
 
 
 
 
 
-
-
-  const trackhtml0 ='<li class="uav-list-item"><div class="uav-item-header">\
-          <span class="uav-item-status">';
-          const trackhtml1 ='</span>\
-          <button class="btn uav-btn-more" id="';
-          const trackhtml1_1='">Подробнее</button>\
-          </div>\
-          <div class="uav-item-body">';
-         // <span class="uav-item-row uav-item-reg">123123123123ABBB</span>\
-         const trackhtml2='<span class="uav-item-row uav-item-flight"><span class="uav-item-desc">Наименование</span>';
-         const trackhtml3 ='</span>\
-          <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Дата создания</span>';
-          const trackhtml4='</span>\
-                           </div>\
-                           <div class="uav-item-body" id="';
-          const trackhtml4_2='"></div>';
-          const trackhtml12_2='<input type="hidden" id="';
-          const trackhtml12_3='" value="';
-          const trackhtml12_4='"></input>';
-          const trackhtml13='</li>';
+  static  trackhtml0 =['<li class="uav-list-item"><div class="uav-item-header">  <span class="uav-item-status">',
+          '</span>      <button class="btn uav-btn-more" id="',
+          '">Подробнее</button>          </div>          <div class="uav-item-body">',
+           '<span class="uav-item-row uav-item-flight"><span class="uav-item-desc">Наименование</span>',
+          '</span>  <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Дата создания</span>',
+          '</span>               </div>         <div class="uav-item-body" id="',
+          '"></div>',
+          '<input type="hidden" id="',
+          '" value="',
+          '"></input>',
+          '</li>'];
          
 
   
-     const routhtml1='<p   <div >\
+          static trackrdetal=[['<p   <div >\
    <label style="width: 50px;font-size:12px;"> Duration</label>\
    <label style="width: 50px;font-size:12px;">Zmin</label>\
-  <label style="width: 50px;font-size:12px;" >Zmax</label></div> ';
-  const routhtml1a='<p    <div > <label style="width: 50px;font-size:12px;"> Duration</label> <label style="width: 50px;font-size:12px;">Radius</label>  <label style="width: 50px;font-size:12px;">Zmin</label> <label style="width: 50px;font-size:12px;">Zmax</label> </div> ';
-  const routhtml2='  <div ';
-  const routhtml3='>  <input style="width: 50px;font-size:12px;"  type="number" id="';
-  const routhtml3a='> <input style="width: 50px;font-size:12px;"  type="hidden" id="';
-  const routhtml4='"   value='
-  const routhtml5='></div></p></div>';
-  const routhtml6='></div>';
+  <label style="width: 50px;font-size:12px;" >Zmax</label></div> ',
+  '<p    <div > <label style="width: 50px;font-size:12px;"> Duration</label>\
+   <label style="width: 50px;font-size:12px;">Radius</label>\
+   <label style="width: 50px;font-size:12px;">Zmin</label>\
+   <label style="width: 50px;font-size:12px;">Zmax</label></div> ',
+   '<p    <div><label style="width: 10px;font-size:12px;">*</label>\
+    <label style="width: 50px;font-size:12px;">  Speed</label>\
+    <label style="width: 50px;font-size:12px;"> Z </label>\
+     <label style="width: 50px;font-size:12px;"> Zmin </label>\
+     <label style="width: 50px;font-size:12px;"> Zmax </label> </div> '],
+    ['  <div ','  <div ','  <div '],
+    ['>  <input style="width: 50px;font-size:12px;"  type="number" id="',
+    '> <input style="width: 50px;font-size:12px;"  type="hidden" id="',''],
+    ['"   value=','"   value=','"   value='],
+    ['></div></p></div>','></div></p></div>','></div></p></div>']
+    ['></div>','></div>','></div>']];
 
 
-  const routhtml1='<p    <div><label style="width: 10px;font-size:12px;">*</label> <label style="width: 50px;font-size:12px;">  Speed</label>  <label style="width: 50px;font-size:12px;"> Z </label>  <label style="width: 50px;font-size:12px;"> Zmin </label>  <label style="width: 50px;font-size:12px;"> Zmax </label> </div> ';
-  const routhtml2='  <div  ';
-  const routhtml8='  <div >  <input  type="checkbox" id="';
-  const routhtml9='" unchecked ';
-  const routhtml3='>  <input style="width: 50px;font-size:12px;" type="number" id="';
-  const routhtml4='"   value=';
-  const routhtml5='></div></p></div>';
-  const routhtml6='></div>'
   
   
+    static routhtml=['  <div >  <input  type="checkbox" id="',
+            '" unchecked '];
+  
+  
+  
 
 
 
@@ -108,34 +79,21 @@
 
 
 
-  const flightrealhtml0 ='<li class="uav-list-item"><div class="uav-item-header">\
-          <span class="uav-item-status">В полёте</span>\
-          <span class="uav-item-desc" id="';
-          const flightrealhtml0_1 ='">Unknow</span>\
-          <button class="btn uav-btn-more" id="';
-          
-          const flightrealhtml0_1_1='">Подробнее</button>\
-           </div>  <div class="uav-item-body" id="';
-          const flightrealhtml0_1_2='">'
-          
-        const flightrealhtml1 ='</div>\
-        <div class="uav-item-footer">\
-          <button class="btn btn-uav-small">Стоп</button>\
-          <button class="btn btn-uav-small" id="';
-          const flightrealhtml2 ='">Возврат</button>\
-          <button class="btn btn-uav-small">Кружиться</button>\
-          <button class="btn btn-uav-small">Посадить</button>\
-        </div>\
-      </li>'
+            static flightrealhtml0 =['<li class="uav-list-item"><div class="uav-item-header">   <span class="uav-item-status">В полёте</span> <span class="uav-item-desc" id="',
+         '">Unknow</span> <button class="btn uav-btn-more" id="',
+         '">Подробнее</button> </div>  <div class="uav-item-body" id="',
+          '">',          
+          '</div>   <div class="uav-item-footer">  <button class="btn btn-uav-small">Стоп</button> <button class="btn btn-uav-small" id="',
+          '">Возврат</button>   <button class="btn btn-uav-small">Кружиться</button> <button class="btn btn-uav-small">Посадить</button> </div></li>'];
 
 
 
 
-  const flightrealhtml0_1_3='<span class="uav-item-row uav-item-flight"><span class="uav-item-desc">Пользователь: ';  
-  const flightrealhtml0_2 ='</span>';
-  const flightrealhtml0_3 ='</span> <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Время старта</span>';
-  const flightrealhtml0_3_1='</span>\
-  <span class="uav-item-row uav-item-date-end"><span class="uav-item-desc">Время финиша</span>';
-  const flightrealhtml0_3_2='</span>';
-  const flightrealhtml0_4='<input type="hidden" id="';
-  const flightrealhtml0_5='"></input>';
+          static  flightrealdet=['<span class="uav-item-row uav-item-flight"><span class="uav-item-desc">Пользователь: ',  
+          '</span>',
+          '</span> <span class="uav-item-row uav-item-date-start"><span class="uav-item-desc">Время старта</span>',
+          '</span>  <span class="uav-item-row uav-item-date-end"><span class="uav-item-desc">Время финиша</span>',
+           '</span>',
+          '<input type="hidden" id="',
+           '"></input>']
+ }
